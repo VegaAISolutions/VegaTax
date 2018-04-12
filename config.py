48d608da72a8617572b171192044fb646998257a
VegaAISolutions <34011241+VegaAISolutions@users.noreply.github.com>
2018-04-12 11:19:37 -0500

    Revert "webapp for Vega tax"
    This reverts commit 6bfcf454cb0f21eccb747fabcdb2dc13ed6b4f60.

--- config.py
+++
@@ -1,4 +0,0 @@
-taxreport_base = 'C:/Users/schre/Downloads/taxapp/'
-reports_db = 'C:/Users/schre/Downloads/taxapp/'
-uploads = 'C:/Users/schre/Downloads/taxapp/uploads/'
-downloads = 'C:/Users/schre/Downloads/taxapp/downloads/'
